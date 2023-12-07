4d4776a9adb17036fb2f8bd16e5c9890ff419791
Giacom0Mancuso <giacomomancuso35@email.com>
2023-12-07 19:12:34 +0100

    work done

--- src/org/lessons/java/christmas/Lista.java
+++ src/org/lessons/java/christmas/Lista.java
@@ -3,10 +3,11 @@ package org.lessons.java.christmas;
 import java.util.Scanner;
 
 public class Lista {
-    private int size;
+    private int size = 0;
+
     private String[] list = new String[size];
 
-    public Lista(){
+    public Lista(int size, String[] list){
         this.list = list;
         this.size = size;
     }
@@ -15,49 +16,67 @@ public class Lista {
         return list;
     }
 
+    public void setList(String[] list) {
+        this.list = list;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     public int getSize() {
         return size;
     }
 
-    public void writeIn(){
+    public void writeIn(int size, String list[]){
         String newGift;
+        list = new String[size];
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i<=list.length; i++){
+        for (int i = 0; i< size; i++){
+            boolean notDone = true;
             System.out.println("Enter a new gift");
             newGift = scanner.nextLine();
             if (repetitionCheck(newGift, list)){
                 list[i] = newGift;
             }
-            else{
+            else if (!repetitionCheck(newGift, list)){
                 System.out.println("You have already enter this gift");
-                size = size - 1;
+                i = i - 1;
+                notDone = false;
             }
-            if (i == list.length) {
+            if (notDone) {
+                System.out.println("the size of the lis is of " + size);
                 System.out.println("Do you want to add more? y/n");
                 String yesOrNot = scanner.nextLine();
                 if (yesOrNot.equals("yes") || yesOrNot.equals("y")) {
-                    size++;
+                    size = size + 1;
+                    String[] temp = new String[size];
+                    for (int j = 0; j < list.length; j++) {
+                        temp[j] = list[j];
+                    }
+                    list = temp;
                 }
             }
         }
+        printArray(list);
     }
-
     private Boolean repetitionCheck (String Gift, String[] list){
-        for (int i = 0; i<list.length; i++){
+        for (int i = 0; i< list.length; i++){
             if (Gift.equals(list[i])){
                 return false;
             }
         }
         return true;
     }
-
-    @Override
-    public String toString(){
+    public void printArray(String[] list){
         String info = "";
-        for (int i = 0; i<list.length; i++){
-            info = info + list[i] + ", ";
+        for (int i = 0; i < list.length; i++) {
+            if (i< list.length-1) {
+                info = info + list[i] + ", ";
+            }else{
+                info = info + list[i] + ".";
+            }
         }
-        return info;
+        System.out.println(info);
     }
-
 }
--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,2 +1,10 @@
-package org.lessons.java.christmas;public class Main {
+package org.lessons.java.christmas;
+
+public class Main {
+    public static void main(String[] args) {
+        int size = 1;
+        String[] list = new String[size];
+        Lista userList = new Lista(size, list);
+        userList.writeIn(size, list);
+    }
 }
